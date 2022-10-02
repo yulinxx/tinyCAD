879aa0151d32ebb40648bfcb0713b35be30bf9ac
xx <xx@ubuntu.com>
2022-10-02 11:28:49 +0800

    窗口初始化等移至类中进行封装

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,19 @@
 #include <glad/glad.h> // glad.h 必须放在glfw3.h 或者glut.h文件之前。
 #include <GLFW/glfw3.h>
 
-#include <iostream>
-#include <random>
-#include <functional>
-
-#include "shader.h"
-#include "DataDefine.h"
-
 #include "window.h"
 
 /////////////////////////////////////////////////////////////
 int main()
 {
-    window w;
+    window wnd;
     const int w = 1200;
     const int h = 800;
     std::string wndName = "tinyCAD";
-    if(!w.initWnd(1200, 800, wndName))
+    if(!wnd.initWnd(w, h, wndName))
         return -1;
     
-    w.run();
+    wnd.run();
     
     return 0;
 }
