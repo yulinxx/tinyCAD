d2e9b1329e92d5cf7e228a62ca586ffd2b6b7ae6
yulinxx <yulinxx@gmail.com>
2022-10-15 22:37:21 +0800

    重载 = 以及 ==

--- include/DataDefine.h
+++ include/DataDefine.h
@@ -12,6 +12,25 @@ struct Pt
         this->z = z;
     }
 
+    Pt& operator=(const Pt& pt)
+    {
+        this->x = pt.x;
+        this->y = pt.y;
+        this->z = pt.z;
+        return *this;
+    }
+    
+    bool operator==(const Pt& pt)
+    {
+        if(fabs(this->x - x) < 0.0001
+        && fabs(this->y - y) < 0.0001
+        && fabs(this->z - z) < 0.0001 )
+        {
+            return true;
+        }
+        return false;
+    }
+    
     double x = 0.0;
     double y = 0.0;
     double z = 0.0;
