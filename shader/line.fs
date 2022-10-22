2d4fea3467c3550d90c79e62c6b66565bf6a76d1
yulinxx <yulinxx@gmail.com>
2022-10-22 11:40:10 +0800

    绘制比较粗劣的虚线

--- shader/line.fs
+++ shader/line.fs
@@ -1,10 +1,36 @@
 #version 450 core
 out vec4 FragColor;
 
-// uniform mat4 matColor = mat4(1.0f, 0.5f, 0.2f, 1.0f);
-uniform vec4 color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+uniform mat4 camera;
+uniform sampler2D depthbuf;
 
-void main()
-{
-    FragColor = color;
+in vec3 frag_pos;
+flat in vec3 vert_pos;
+uniform float scatter_size = 0.2;
+
+uniform vec4 color = vec4(1.0f, 0.1f, 0.0f, 1.0f);
+
+
+float getShadow(vec3 pos) {
+    vec4 p = camera * vec4(pos, 1.0);
+    p.xyz /= p.w;
+    p.xyz = p.xyz * 0.5 + 0.5;
+    float f = 3.0;
+    float castmask = max(f * (p.x), 0.0) * max(f * (1.0 - p.x), 0.0) * max(f * (p.y), 0.0) * max(f * (1.0 - p.y), 0.0);
+    castmask = min(castmask, 1.0);
+    float d = texture(depthbuf, p.xy).r;
+    d = 0.3;
+    return castmask * max(sign(p.z - d - 0.00001), 0.0);
+}
+
+void main() {
+    float shadow = getShadow(frag_pos);
+    float distance = length(vert_pos - frag_pos);
+    
+    // float alpha = 1.0;
+    float alpha = 1.0 - shadow + shadow * max(0.0, sign(mod(distance, scatter_size) - scatter_size*0.5));
+    if(int(frag_pos.x) % 3 == 0)
+        alpha = 0;
+
+    FragColor = vec4(color.x, color.y, color.z, alpha);
 }
--- shader/line.vs
+++ shader/line.vs
@@ -5,8 +5,13 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 projection;
 
+out vec3 frag_pos;
+flat out vec3 vert_pos;
 
 void main()
 {
     gl_Position = projection * view * model * vec4(aPos.x, aPos.y, aPos.z, 1.0);
+
+    vert_pos = aPos;
+    frag_pos = aPos;
 }
